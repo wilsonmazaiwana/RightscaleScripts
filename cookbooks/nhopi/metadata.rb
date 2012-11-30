f3c3e478d9fcbf05f052b420c8368f7c38859409
wilsonmazaiwana <wilson.mazaiwana@moshen.com>
2012-11-30 17:07:38 +0000

    added a description to the whole thing...(forgot)

---
+++ cookbooks/nhopi/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Moshen ltd"
+maintainer_email "wilson.mazaiwana@moshen.com"
+license          "All rights reserved"
+description      "Installs/Configures nhopi"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+
+recipe "nhopi::default","cooks amqp for php (and more stuff, other basic stuff)"
